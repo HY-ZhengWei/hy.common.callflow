bd12eb03953b7668782cdf389ab291080b987daa
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-07-04 16:25:08 +0800

    1.2.8
    1. 测试用例：占位符的举例。
    2. 升级：修正全路径解释的问题，在替换子组方法路径时，用replaceFirst替换replaceAll 方法

--- src/test/java/org/hy/common/callflow/junit/cflow024/program/Program.java
+++ src/test/java/org/hy/common/callflow/junit/cflow024/program/Program.java
@@ -19,10 +19,90 @@ import org.hy.common.Help;
 public class Program
 {
     
-    public void method_Info(Map<String ,Object> i_Context)
+    /** 字符类型 */
+    private String  valueString;
+    
+    /** 整数类型 */
+    private Integer valueInteger;
+    
+    /** 复合对象 */
+    private Program myself;
+    
+    
+    
+    public Program method_Info(Map<String ,Object> i_Context)
     {
         System.out.println(Date.getNowTime().getFullMilli() + " call method_Info.");
         Help.print(i_Context);
+        
+        Program v_Ret = new Program();
+        v_Ret.setValueString("XCFlow_MouldingLine_1_GetState");
+        v_Ret.setValueInteger(123456);
+        
+        v_Ret.setMyself(new Program());
+        v_Ret.getMyself().setValueString("XPLC_MouldingLine");
+        
+        return v_Ret;
+    }
+    
+    
+    /**
+     * 获取：字符类型
+     */
+    public String getValueString()
+    {
+        return valueString;
+    }
+
+    
+    /**
+     * 设置：字符类型
+     * 
+     * @param i_ValueString 字符类型
+     */
+    public void setValueString(String i_ValueString)
+    {
+        this.valueString = i_ValueString;
+    }
+
+    
+    /**
+     * 获取：整数类型
+     */
+    public Integer getValueInteger()
+    {
+        return valueInteger;
+    }
+
+    
+    /**
+     * 设置：整数类型
+     * 
+     * @param i_ValueInteger 整数类型
+     */
+    public void setValueInteger(Integer i_ValueInteger)
+    {
+        this.valueInteger = i_ValueInteger;
+    }
+
+    
+    /**
+     * 获取：复合对象
+     */
+    public Program getMyself()
+    {
+        return myself;
+    }
+
+    
+    /**
+     * 设置：复合对象
+     * 
+     * @param i_Myself 复合对象
+     */
+    public void setMyself(Program i_Myself)
+    {
+        this.myself = i_Myself;
     }
     
 }
