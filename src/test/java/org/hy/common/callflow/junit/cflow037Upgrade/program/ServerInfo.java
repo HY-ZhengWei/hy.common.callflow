7aa45ad37b3fc84951b4f390901879644527a7b6
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-10-17 09:46:36 +0800

    1.5.8
    1.添加：集群批量配置设备直连IP地址

--- src/test/java/org/hy/common/callflow/junit/cflow037Upgrade/program/ServerInfo.java
+++ src/test/java/org/hy/common/callflow/junit/cflow037Upgrade/program/ServerInfo.java
@@ -1,5 +1,7 @@
 package org.hy.common.callflow.junit.cflow037Upgrade.program;
 
+import org.hy.common.Help;
+
 
 
 
@@ -17,6 +19,15 @@ public class ServerInfo
     /** 服务器IP地址 */
     private String ip;
     
+    /** 服务器的设备直联IP地址 */
+    private String ipDevice1;
+    
+    /** 服务器的设备直联IP地址 */
+    private String ipDevice2;
+    
+    /** 服务器的设备直联IP地址 */
+    private String ipDevice3;
+    
     /** 服务器用户名称 */
     private String user;
     
@@ -25,6 +36,9 @@ public class ServerInfo
     
     /** 服务器类型 */
     private String type;
+    
+    /** 微服务类型 */
+    private String msType;
 
     
     
@@ -48,6 +62,93 @@ public class ServerInfo
     }
 
     
+    /**
+     * 获取：服务器的设备直联IP地址
+     */
+    public String getIpDevice1Help()
+    {
+        return Help.NVL(ipDevice1 ,"192.168.99.101");
+    }
+    
+    
+    /**
+     * 获取：服务器的设备直联IP地址
+     */
+    public String getIpDevice1()
+    {
+        return ipDevice1;
+    }
+
+    
+    /**
+     * 设置：服务器的设备直联IP地址
+     * 
+     * @param i_IpDevice1 服务器的设备直联IP地址
+     */
+    public void setIpDevice1(String i_IpDevice1)
+    {
+        this.ipDevice1 = i_IpDevice1;
+    }
+
+    
+    /**
+     * 获取：服务器的设备直联IP地址
+     */
+    public String getIpDevice2Help()
+    {
+        return Help.NVL(ipDevice2 ,"192.168.99.102");
+    }
+    
+    
+    /**
+     * 获取：服务器的设备直联IP地址
+     */
+    public String getIpDevice2()
+    {
+        return ipDevice2;
+    }
+
+    
+    /**
+     * 设置：服务器的设备直联IP地址
+     * 
+     * @param i_IpDevice2 服务器的设备直联IP地址
+     */
+    public void setIpDevice2(String i_IpDevice2)
+    {
+        this.ipDevice2 = i_IpDevice2;
+    }
+
+    
+    /**
+     * 获取：服务器的设备直联IP地址
+     */
+    public String getIpDevice3Help()
+    {
+        return Help.NVL(ipDevice3 ,"192.168.99.103");
+    }
+    
+    
+    /**
+     * 获取：服务器的设备直联IP地址
+     */
+    public String getIpDevice3()
+    {
+        return ipDevice3;
+    }
+
+    
+    /**
+     * 设置：服务器的设备直联IP地址
+     * 
+     * @param i_IpDevice3 服务器的设备直联IP地址
+     */
+    public void setIpDevice3(String i_IpDevice3)
+    {
+        this.ipDevice3 = i_IpDevice3;
+    }
+
+
     /**
      * 获取：服务器用户名称
      */
@@ -105,6 +206,26 @@ public class ServerInfo
     public void setType(String i_Type)
     {
         this.type = i_Type;
+    }
+
+    
+    /**
+     * 获取：微服务类型
+     */
+    public String getMsType()
+    {
+        return msType;
+    }
+
+    
+    /**
+     * 设置：微服务类型
+     * 
+     * @param i_MsType 微服务类型
+     */
+    public void setMsType(String i_MsType)
+    {
+        this.msType = i_MsType;
     }
     
 }
