65aa85f0898de0124314f8bffe7d52be8e92fa95
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-11-06 09:55:42 +0800

    1.6.5
    1. 添加：测试单元：无限循环

---
+++ src/test/java/org/hy/common/callflow/junit/cflow04InfiniteLoop/JU_CFlow040.java
@@ -0,0 +1,98 @@
+package org.hy.common.callflow.junit.cflow04InfiniteLoop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hy.common.Date;
+import org.hy.common.Return;
+import org.hy.common.callflow.CallFlow;
+import org.hy.common.callflow.execute.ExecuteResult;
+import org.hy.common.callflow.junit.JUBase;
+import org.hy.common.callflow.junit.cflow04InfiniteLoop.program.Program;
+import org.hy.common.callflow.node.NodeConfig;
+import org.hy.common.xml.XJava;
+import org.hy.common.xml.annotation.XType;
+import org.hy.common.xml.annotation.Xjava;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+
+
+
+
+/**
+ * 测试单元：编排引擎040：无限循环
+ *
+ * @author      ZhengWei(HY)
+ * @createDate  2025-10-30
+ * @version     v1.0
+ */
+@Xjava(value=XType.XML)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) 
+public class JU_CFlow040 extends JUBase
+{
+    
+    private static boolean $isInit = false;
+    
+    
+    
+    public JU_CFlow040() throws Exception
+    {
+        if ( !$isInit )
+        {
+            $isInit = true;
+            XJava.parserAnnotation(this.getClass().getName());
+        }
+    }
+    
+    
+    
+    @Test
+    public void test_CFlow040()
+    {
+        this.test_CFlow040_Inner();
+    }
+    
+    
+    
+    private void test_CFlow040_Inner()
+    {
+        // 初始化被编排的执行对象方法
+        XJava.putObject("XProgram" ,new Program());
+        
+        // 获取编排中的首个元素
+        NodeConfig          v_Node    = (NodeConfig) XJava.getObject("XNode_CF040_Start");
+        Map<String ,Object> v_Context = new HashMap<String ,Object>();
+        
+        // 执行前的静态检查（关键属性未变时，check方法内部为快速检查）
+        Return<Object> v_CheckRet = CallFlow.getHelpCheck().check(v_Node);
+        if ( !v_CheckRet.get() )
+        {
+            System.out.println(v_CheckRet.getParamStr());  // 打印不合格的原因
+            return;
+        }
+        
+        ExecuteResult v_Result = CallFlow.execute(v_Node ,v_Context);
+        if ( v_Result.isSuccess() )
+        {
+            System.out.println("Success");
+        }
+        else
+        {
+            System.out.println("Error XID = " + v_Result.getExecuteXID());
+            v_Result.getException().printStackTrace();
+        }
+        
+        // 打印执行路径
+        ExecuteResult v_FirstResult = CallFlow.getFirstResult(v_Context);
+        System.out.println(CallFlow.getHelpLog().logs(v_FirstResult));
+        System.out.println("整体用时：" + Date.toTimeLenNano(v_Result.getEndTime() - v_Result.getBeginTime()) + "\n");
+        
+        // 导出
+        System.out.println(CallFlow.getHelpExport().export(v_Node));
+        
+        toJson(v_Node);
+    }
+    
+}
---
+++ src/test/java/org/hy/common/callflow/junit/cflow04InfiniteLoop/program/Program.java
@@ -0,0 +1,40 @@
+package org.hy.common.callflow.junit.cflow04InfiniteLoop.program;
+
+import java.util.Map;
+
+import org.hy.common.Date;
+import org.hy.common.Help;
+
+
+
+
+
+/**
+ * 模拟被编排的程序 
+ *
+ * @author      ZhengWei(HY)
+ * @createDate  2025-10-30
+ * @version     v1.0
+ */
+public class Program
+{
+    
+    public void start()
+    {
+        System.out.println(Date.getNowTime().getFull() + " 开始");
+    }
+    
+    
+    
+    public void loop(Map<String ,Object> io_Context)
+    {
+        Help.print(io_Context);
+    }
+    
+    
+    public void finish()
+    {
+        System.out.println(Date.getNowTime().getFull() + " 完成");
+    }
+    
+}
